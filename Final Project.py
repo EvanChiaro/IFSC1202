931d07848b232bfa44f06e5b0d171ba71779d71f
EvanChiaro <emchiaro@ualr.edu>
2022-05-02 15:44:23 +0000

    add

--- Final Project.py
+++ Final Project.py
@@ -8,33 +8,32 @@ class Sketch():
         self.Pen = pen
         self.Canvas = []
     def init(self,size):
-        a = []
-        direcfile = open("Cshape.txt")
-        direction = direcfile.readline()
-        size = int(direction)
+        self.Size = size
+        x = []
+        #for i in range(size):
+        #    x.append(" ")
+      #     self.Canvas.append(x)
         for i in range(size):
-            a.append(" ")
-        for i in range(size):
-            self.Canvas.append(a)
-    def printsketch():
+            self.Canvas.append([" "] * size)
+    def printsketch(self):
         print("+", end="")
         for i in range(self.Size):
             print("-", end="")
         print("+")
-        for i in range(len(canvas)):
+        for i in range(len(self.Canvas)):
             print("|", end = "")
-            for j in range(0, len(a), -1):
-                print(self.Canvas[i][j], end = "")
+            for j in range(self.Size):
+                print(self.Canvas[-i-1][j], end = "")
             print('|')
         print("+", end="")
         for i in range(self.Size):
             print("-", end="")
         print("+")
-    def penup():
+    def penup(self):
         self.Pen = "U"
-    def pendown():
+    def pendown(self):
         self.Pen = "D"
-    def turnleft():
+    def turnleft(self):
         if self.Direction == "U":
             self.Direction = "L"
         elif self.Direction == "L":
@@ -43,7 +42,7 @@ class Sketch():
             self.Direction = "R"
         elif self.Direction == "R":
             self.Direction = "U"
-    def turnright():
+    def turnright(self):
         if self.Direction == "U":
             self.Direction = "R"
         elif self.Direction == "L":
@@ -53,8 +52,10 @@ class Sketch():
         elif self.Direction == "R":
             self.Direction = "D"
     def move(self,distance):
-        distance = distane.split(',')
-        for i in range(int(distance)):
+        distance = distance.split(',')
+        for i in range(int(distance[1])):
+            if self.Pen == "D":
+                self.Canvas[self.Ypos][self.Xpos] = '*'
             if self.Direction == "U":
                 self.Ypos += 1
             elif self.Direction == "D":
@@ -63,5 +64,34 @@ class Sketch():
                 self.Xpos += 1
             elif self.Direction == "L":
                 self.Xpos -= 1
-            if self.Pen == "U":
-                self.Caanvas
+
+
+
+#main function
+instructfile = open("Cshape.txt")
+length = instructfile.readlines()
+instructfile.close()
+instructfile = open("Cshape.txt")
+instruct = instructfile.readline()
+size = int(instruct)
+sketch1 = Sketch()
+sketch1.init(size)
+for i in range(len(length)):
+    instruct = instructfile.readline()
+    instruct = instruct.strip()
+    ins = instruct.split(',')
+    if ins[0] == "1":
+        sketch1.penup()
+    elif ins[0] == "2":
+        sketch1.pendown()
+    elif ins[0] == "3":
+        sketch1.turnright()
+    elif ins[0] == "4":
+        sketch1.turnleft()
+    elif ins[0] == "5":
+        sketch1.move(instruct)
+    elif ins[0] == "6":
+        sketch1.printsketch()
+instructfile.close()
+
+print("X = {}  Y = {}  Direction = {}".format(sketch1.Xpos, sketch1.Ypos,sketch1.Direction))
